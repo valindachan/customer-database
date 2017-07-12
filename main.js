cd02a427995e72d39f6e2ed5745660903adc0c3f
Valinda Chan <valinda.chan@gmail.com>
2017-07-12 16:38:23 -0400

    Created the three rows for all the profiles

--- main.js
+++ main.js
@@ -3,47 +3,46 @@ let header = document.getElementById("header")
 header.innerHTML = "Internal Company Directory"
 
 let directory = document.getElementById("directory")
-let row = document.createElement("div")
 
 // console.log(directory)
 
 for (var i = 0; i < 3; i++) {
   // Create a row for the first four profiles
+  let row = document.createElement("div")
   row.classList.add("row")
   directory.appendChild(row)
 
   // Create a row for the first four profiles
   row.classList.add("row")
+  directory.appendChild(row)
+
+  for (var j = 0; j < customers.results.length; j++) {
+    let personInfo = customers.results[j]
+    // let personSlimInfo = `${personInfo.name.first} ${personInfo.name
+    //   .last} ${personInfo.email} ${personInfo.location.street} ${personInfo
+    //   .location.city} ${personInfo.location.state} ${personInfo.location
+    //   .postcode}`
+
+    let picture = personInfo.picture.large
+    let name = `${personInfo.name.first} ${personInfo.name.last}`
+    let email = personInfo.email
+    let address = `${personInfo.location.street} ${personInfo.location
+      .city} ${personInfo.location.state} ${personInfo.location.postcode}`
+    let phone = personInfo.phone
+    let ssn = personInfo.id.ssn
+    // console.log(picture, name, email, address, phone)
+
+    let image = document.createElement("img")
+    image.src = picture
+
+    let nameContent = document.createElement("p")
+    nameContent.classList.add("name")
+    nameContent.innerHTML = name
+
+    row.appendChild(image)
+    row.appendChild(nameContent)
+  }
 }
 // console.log(directory)
-
-for (var i = 0; i < customers.results.length; i++) {
-  let personInfo = customers.results[i]
-  // let personSlimInfo = `${personInfo.name.first} ${personInfo.name
-  //   .last} ${personInfo.email} ${personInfo.location.street} ${personInfo
-  //   .location.city} ${personInfo.location.state} ${personInfo.location
-  //   .postcode}`
-
-  let picture = personInfo.picture.large
-  let name = `${personInfo.name.first} ${personInfo.name.last}`
-  let email = personInfo.email
-  let address = `${personInfo.location.street} ${personInfo.location
-    .city} ${personInfo.location.state} ${personInfo.location.postcode}`
-  let phone = personInfo.phone
-  let ssn = personInfo.id.ssn
-  // console.log(picture, name, email, address, phone)
-
-  let image = document.createElement("img")
-  image.src = picture
-
-  let nameContent = document.createElement("p")
-  nameContent.classList.add("name")
-  nameContent.innerHTML = name
-
-  row.appendChild(image)
-  row.appendChild(nameContent)
-}
-
-directory.appendChild(row)
 
 console.log(directory)
